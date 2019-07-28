908f40a2d3ed97f314fb536b238093e8655d3ac7
RickyChiu1231 <laiyin101020@gmail.com>
2019-07-28 05:47:58 +0000

    shippingaddresslist

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Auth::routes(['verify' => true]);
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
 });
 
 
