f47d13ddfc95a2f43c3b059fe67c9ec82504fef3
RickyChiu1231 <laiyin101020@gmail.com>
2019-07-31 05:13:03 +0000

    backendmanagementCreateEditproduct

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -27,20 +27,6 @@ class ProductsController extends Controller
             ->body($this->grid());
     }
 
-    /**
-     * Show interface.
-     *
-     * @param mixed $id
-     * @param Content $content
-     * @return Content
-     */
-    public function show($id, Content $content)
-    {
-        return $content
-            ->header('Detail')
-            ->description('description')
-            ->body($this->detail($id));
-    }
 
     /**
      * Edit interface.
@@ -52,8 +38,7 @@ class ProductsController extends Controller
     public function edit($id, Content $content)
     {
         return $content
-            ->header('Edit')
-            ->description('description')
+            ->header('Edit Product')
             ->body($this->form()->edit($id));
     }
 
@@ -66,8 +51,7 @@ class ProductsController extends Controller
     public function create(Content $content)
     {
         return $content
-            ->header('Create')
-            ->description('description')
+            ->header('Create Product')
             ->body($this->form());
     }
 
@@ -104,30 +88,6 @@ class ProductsController extends Controller
         return $grid;
     }
 
-    /**
-     * Make a show builder.
-     *
-     * @param mixed $id
-     * @return Show
-     */
-    protected function detail($id)
-    {
-        $show = new Show(Product::findOrFail($id));
-
-        $show->id('Id');
-        $show->title('Title');
-        $show->description('Description');
-        $show->image('Image');
-        $show->on_sale('On sale');
-        $show->rating('Rating');
-        $show->sold_count('Sold count');
-        $show->review_count('Review count');
-        $show->price('Price');
-        $show->created_at('Created at');
-        $show->updated_at('Updated at');
-
-        return $show;
-    }
 
     /**
      * Make a form builder.
@@ -138,14 +98,24 @@ class ProductsController extends Controller
     {
         $form = new Form(new Product);
 
-        $form->text('title', 'Title');
-        $form->textarea('description', 'Description');
-        $form->image('image', 'Image');
-        $form->switch('on_sale', 'On sale')->default(1);
-        $form->decimal('rating', 'Rating')->default(5.00);
-        $form->number('sold_count', 'Sold count');
-        $form->number('review_count', 'Review count');
-        $form->decimal('price', 'Price');
+        $form->text('title', 'Product Name')->rules('required');
+
+        $form->image('image', 'Product Image')->rules('required|image');
+
+        $form->editor('description', 'Product Description')->rules('required');
+
+        $form->radio('on_sale', 'On Sale')->options(['1' => 'Yes', '0'=> 'No'])->default('0');
+
+        $form->hasMany('skus', 'SKU List', function (Form\NestedForm $form) {
+            $form->text('title', 'SKU Name')->rules('required');
+            $form->text('description', 'SKU Description')->rules('required');
+            $form->text('price', 'Price per unit')->rules('required|numeric|min:0.01');
+            $form->text('stock', 'Remaining stock')->rules('required|integer|min:0');
+        });
+
+        $form->saving(function (Form $form) {
+            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+        });
 
         return $form;
     }
